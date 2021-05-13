68612b211768141db69a82c8df0c7eb2358c8833
ArtOfDarkness <icefoxyyy@gmail.com>
2021-05-14 01:06:14 +0300

    AJ_HW

---
+++ homework_20/java/repository/UserRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+	public User findByUserName(String username);
+}
